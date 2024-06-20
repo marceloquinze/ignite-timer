399c9ac836777a762f596d09467ec9704fcb8fda
Marcelo Vieira <marceloquinze@gmail.com>
2024-06-20 16:08:31 -0300

    Added Header and Layouts

---
+++ src/components/Header/index.tsx
@@ -0,0 +1,20 @@
+import { Scroll, Timer } from 'phosphor-react'
+import igniteLogo from '../../assets/ignite-logo.svg'
+import { HeaderContainer } from './styles'
+import { NavLink } from 'react-router-dom'
+
+export function Header() {
+  return (
+    <HeaderContainer>
+      <img src={igniteLogo} alt="" />
+      <nav>
+        <NavLink to="/" title="Timer">
+          <Timer size={24} />
+        </NavLink>
+        <NavLink to="/history" title="History">
+          <Scroll size={24} />
+        </NavLink>
+      </nav>
+    </HeaderContainer>
+  )
+}
